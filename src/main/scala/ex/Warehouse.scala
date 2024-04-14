be1ce8a847df1c960ab2bd171c699cf2e37f5ef3
Mattia Passeri <mattia.passeri2@studio.unibo.it>
2024-04-14 12:51:38 +0200

    Completed exercise 2

--- src/main/scala/ex/Warehouse.scala
+++ src/main/scala/ex/Warehouse.scala
@@ -2,76 +2,87 @@ package ex
 
 import util.Optionals.Optional
 import util.Sequences.*
+
 trait Item:
   def code: Int
   def name: String
   def tags: Sequence[String]
 
 object Item:
-  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ???
+  def apply(code: Int, name: String, tags: String*): Item =
+    ItemImpl(code, name, Sequence(tags*))
 
-/**
- * A warehouse is a place where items are stored.
- */
+  private case class ItemImpl(code: Int, name: String, tags: Sequence[String]) extends Item
+
+/** A warehouse is a place where items are stored.
+  */
 trait Warehouse:
-  /**
-   * Stores an item in the warehouse.
-   * @param item the item to store
-   */
+  /** Stores an item in the warehouse.
+    * @param item
+    *   the item to store
+    */
   def store(item: Item): Unit
-  /**
-   * Searches for items with the given tag.
-   * @param tag the tag to search for
-   * @return the list of items with the given tag
-   */
+
+  /** Searches for items with the given tag.
+    * @param tag
+    *   the tag to search for
+    * @return
+    *   the list of items with the given tag
+    */
   def searchItems(tag: String): Sequence[Item]
-  /**
-   * Retrieves an item from the warehouse.
-   * @param code the code of the item to retrieve
-   * @return the item with the given code, if present
-   */
+
+  /** Retrieves an item from the warehouse.
+    * @param code
+    *   the code of the item to retrieve
+    * @return
+    *   the item with the given code, if present
+    */
   def retrieve(code: Int): Optional[Item]
-  /**
-   * Removes an item from the warehouse.
-   * @param item the item to remove
-   */
+
+  /** Removes an item from the warehouse.
+    * @param item
+    *   the item to remove
+    */
   def remove(item: Item): Unit
-  /**
-   * Checks if the warehouse contains an item with the given code.
-   * @param itemCode the code of the item to check
-   * @return true if the warehouse contains an item with the given code, false otherwise
-   */
+
+  /** Checks if the warehouse contains an item with the given code.
+    * @param itemCode
+    *   the code of the item to check
+    * @return
+    *   true if the warehouse contains an item with the given code, false otherwise
+    */
   def contains(itemCode: Int): Boolean
 end Warehouse
 
 object Warehouse:
-  def apply(): Warehouse = ???
+  def apply(): Warehouse = WarehouseImpl()
+
+  private class WarehouseImpl extends Warehouse:
+    private var items: Sequence[Item] = Sequence.Nil()
+
+    override def store(item: Item): Unit = items = items.concat(Sequence(item))
+
+    override def contains(itemCode: Int): Boolean = items.map(_.code).contains(itemCode)
+
+    override def searchItems(tag: String): Sequence[Item] = items.filter(_.tags.contains(tag))
+
+    override def retrieve(code: Int): Optional[Item] = items.find(_.code == code)
+
+    override def remove(item: Item): Unit = items = items.filter(_ != item)
 
 @main def mainWarehouse(): Unit =
-  val warehouse = Warehouse()
-
-  val dellXps = Item(33, "Dell XPS 15", Sequence("notebook"))
-  val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
-  val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
-
-  warehouse.contains(dellXps.code) // false
+  val warehouse    = Warehouse()
+  val dellXps      = Item(33, "Dell XPS 15", "notebook")
+  val dellInspiron = Item(34, "Dell Inspiron 13", "notebook")
+  val xiaomiMoped  = Item(35, "Xiaomi S1", "moped", "mobility")
+  assert(!warehouse.contains(dellXps.code))
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
-  warehouse.contains(dellXps.code) // true
+  assert(warehouse.contains(dellXps.code))
   warehouse.store(dellInspiron) // side effect, add dell Inspiron to the warehouse
-  warehouse.store(xiaomiMoped) // side effect, add xiaomi moped to the warehouse
-  warehouse.searchItems("mobility") // Sequence(xiaomiMoped)
-  warehouse.searchItems("notebook") // Sequence(dellXps, dell Inspiron)
-  warehouse.retrieve(11) // None
-  warehouse.retrieve(dellXps.code) // Just(dellXps)
+  warehouse.store(xiaomiMoped)  // side effect, add xiaomi moped to the warehouse
+  assert(warehouse.searchItems("mobility") == Sequence(xiaomiMoped))
+  assert(warehouse.searchItems("notebook") == Sequence(dellXps, dellInspiron))
+  assert(warehouse.retrieve(11) == Optional.Empty())
+  assert(warehouse.retrieve(dellXps.code) == Optional.Just(dellXps))
   warehouse.remove(dellXps) // side effect, remove dell xps from the warehouse
-  warehouse.retrieve(dellXps.code) // None
-
-/** Hints:
- * - Implement the Item with a simple case class
- * - Implement the Warehouse keeping a private Sequence of items
- * - Start implementing contains and store
- * - Implement searchItems using filter and contains
- * - Implement retrieve using find
- * - Implement remove using filter
- * - Refactor the code of Item accepting a variable number of tags (hint: use _*)
-*/
+  assert(warehouse.retrieve(dellXps.code) == Optional.Empty())
